a10386d99236e86be53cdd3ebaf8538aa71952bb
Arden Sinclair <general@ardensinclair.com>
2022-04-23 17:23:06 -0700

    Rename to better match purpose

--- benches/lexer_bench.rs
+++ benches/lexer_bench.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use rust_playground::lexer::{Lexer, Token};
+use lisp_interpreter::lexer::{Lexer, Token};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("lex", |b| {
